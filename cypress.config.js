801db6d2fb78c56364b1b0b9fe57134866133a98
fabiano00760 <fabiano00760@yahoo.com>
2023-05-29 00:59:21 -0300

    primeiro commit

---
+++ cypress.config.js
@@ -0,0 +1,21 @@
+const { defineConfig } = require('cypress')
+
+module.exports = defineConfig({
+  reporter: 'mochawesome',
+  reporterOptions: {
+    reportDir: 'cypress/report/mochawesome-report',
+    overwrite: true,
+    html: true,
+    json: false,
+    timestamp: 'mmddyyyy_HHMMss',
+  },
+  projectId: "5rahhk",
+  e2e: {
+    // We've imported your old cypress plugins here.
+    // You may want to clean this up later by importing these.
+    setupNodeEvents(on, config) {
+      return require('./cypress/plugins/index.js')(on, config)
+    },
+    specPattern: 'cypress/e2e/**/*.{js,jsx,ts,tsx}',
+  },
+})
